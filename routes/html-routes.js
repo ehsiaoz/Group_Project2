bb396469304f2a3bc3c68fb53449c8f23a594152
en <ehsiaoz@h2cap.com>
2017-02-07 20:39:28 -0600

    bizListing route and single biz route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,10 +26,37 @@ module.exports = function(app) {
     });
   });
 
+  // add route loads the add.html page,
+  // where users can enter new characters to the db
+  app.get("/biz", function(req, res) {
+    // res.sendFile(path.join(__dirname + "/../public/add.html"));
+    db.Biz.findAll({}).then(function(data){
+      console.log("data from DB: ", data);
+      var businesses = { businesses: data};
+      console.log("Businesses (hbsObject): ", businesses);
+      res.render('bizListings', businesses);
+    });
+  });
+
+
   // all route loads the all.html page,
   // where all characters in the db are displayed
-  app.get("/biz/?id", function(req, res) {
+  app.get("/biz/:biz_id", function(req, res) {
     // res.sendFile(path.join(__dirname + "/../public/all.html"));
+
+    db.Biz.findById(req.params.biz_id)
+    // db.Biz.findOne({
+    //   where: {
+    //     id: req.params.biz_id
+    //   }
+    // }
+  // )
+  .then(function(data){
+      console.log("data from DB: ", data);
+      var business = { business: data};
+      console.log("Business (hbsObject): ", business);
+      res.render('biz', business);
+    });
   });
 
 };
