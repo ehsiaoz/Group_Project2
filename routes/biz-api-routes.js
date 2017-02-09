7d662ae0c674355b4c2e9a11e68596f5755c2be9
Harry Saito <hhsaito@gmail.com>
2017-02-09 17:18:41 -0600

    Adde auto lat/long.

--- routes/biz-api-routes.js
+++ routes/biz-api-routes.js
@@ -3,6 +3,7 @@
 // Dependencies
 // =============================================================
 var db = require("../models");
+var geocoder = require("geocoder");
 
 // Routes
 // =============================================================
@@ -11,23 +12,36 @@ module.exports = function(app) {
   // Create a new businesseses
   app.post("/biz", function(req, res) {
     var Biz = req.body;
-	console.log("biz: ", Biz);
-	console.log("biz_name: ", Biz.biz_name);
-	console.log("biz_desc: ", Biz.biz_desc);
+  	console.log("biz: ", Biz);
+  	console.log("biz_name: ", Biz.biz_name);
+  	console.log("biz_desc: ", Biz.biz_desc);
+    var geocode_addr = Biz.biz_street + ', ' + Biz.biz_city + ', ' + Biz.biz_state;
 
-		db.Biz.create({
-			biz_name: Biz.biz_name,
-			biz_desc: Biz.biz_desc,
-      biz_cat: Biz.biz_cat,
-      biz_image: Biz.biz_image,
-      biz_street: Biz.biz_street,
-      biz_city: Biz.biz_city,
-      biz_state: Biz.biz_state,
-      biz_lat: Biz.biz_lat,
-      biz_long: Biz.biz_long
-		}).then(function(data) {
-		res.redirect('/biz');
-	});
+    geocoder.geocode(geocode_addr, function(err, data) {
+      console.log(JSON.stringify(data.results[0].geometry.location, null, 2));
+      var biz_latitude = data.results[0].geometry.location.lat;
+      var biz_longitude = data.results[0].geometry.location.lng;
+      console.log('longitude: ', biz_longitude, '; latitude: ', biz_latitude);
+
+      createBiz(biz_latitude, biz_longitude);
+
+    });
+    function createBiz(biz_latitude, biz_longitude) {
+
+      db.Biz.create({
+        biz_name: Biz.biz_name,
+        biz_desc: Biz.biz_desc,
+        biz_cat: Biz.biz_cat,
+        biz_image: Biz.biz_image,
+        biz_street: Biz.biz_street,
+        biz_city: Biz.biz_city,
+        biz_state: Biz.biz_state,
+        biz_lat: biz_latitude,
+        biz_long: biz_longitude
+      }).then(function(data) {
+        res.redirect('/biz');
+      });
+    }
 
   });
 
