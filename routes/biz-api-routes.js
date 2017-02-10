1a3c57ec6168eb3d543e16c4dc455dce573d4836
en <ehsiaoz@h2cap.com>
2017-02-10 12:23:38 -0600

    added dropdown on create biz page showing existing categories....still unable to associate the existing category id with the biz

--- routes/biz-api-routes.js
+++ routes/biz-api-routes.js
@@ -11,24 +11,25 @@ module.exports = function(app) {
   // Create a new businesseses
   app.post("/biz", function(req, res) {
     var Biz = req.body;
+    console.log(Biz);
 
-		db.Biz.create({
-			biz_name: Biz.biz_name,
-			biz_desc: Biz.biz_desc,
-      biz_cat: Biz.biz_cat,
-      biz_image: Biz.biz_image,
-      biz_street: Biz.biz_street,
-      biz_city: Biz.biz_city,
-      biz_state: Biz.biz_state,
-      biz_lat: Biz.biz_lat,
-      biz_long: Biz.biz_long,
-      Category: {
-        cat_name: Biz.new_cat
-      }
-    }, {
-      include: [db.Category]
-		})
-
+      db.Biz.create({
+  			biz_name: Biz.biz_name,
+  			biz_desc: Biz.biz_desc,
+        biz_cat: Biz.biz_cat,
+        biz_image: Biz.biz_image,
+        biz_street: Biz.biz_street,
+        biz_city: Biz.biz_city,
+        biz_state: Biz.biz_state,
+        biz_lat: Biz.biz_lat,
+        biz_long: Biz.biz_long,
+        Category: {
+          cat_name: Biz.new_cat
+        }
+      }, {
+        include: [db.Category]
+  		})
+      
     .then(function(data) {
 		    res.redirect('/biz');
 	  });
