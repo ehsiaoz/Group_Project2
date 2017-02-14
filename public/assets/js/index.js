f660a6dd597a3455272786ecef06c7ae0fce33ed
nmsb10 <j-nagatani@u.northwestern.edu>
2017-02-14 00:29:54 -0600

    awesome deals now populated. check it out!

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// document.querySelector("body").style.backgroundImage = "url(./assets/img/" + backgroundImages[Math.floor(Math.random()*backgroundImages.length)]+ ")";
 	// spinIcon('search-icon');
 	if(document.getElementById('deal-container-ind')){
-		//populateIndBusDeals(3);
+		populateIndBusDeals(3);
 	}
 });
 
@@ -17,60 +17,105 @@ function spinIcon(icon){
 	});
 }
 
-//function to add howMany deals to the individual business page deals section
+//function to add howMany (randomly fabricated) deals to the individual business page deals section
 function populateIndBusDeals(howMany){
 	var imagesArray = dealImages.slice(0);
+	var titleAdj = ['hipster', 'hand made', 'unforgettable', 'extravagant', 'unique', 'personalized', 'custom', 'unrivaled', 'fresh', 'stylish', 'premium', 'upgraded', 'exclusive', 'modern'];
+	var titleNoun = ['fried chicken', 'retreat', 'service', 'opportunity', 'luxury', 'experience', 'style', 'relaxation', 'package'];
 	for(var i = 0; i < howMany; i ++){
+		console.log(imagesArray);
 		var selectedImage = Math.floor(Math.random()*imagesArray.length);
 		var deal = document.createElement('div');
 		deal.className = 'deal-panel';
-		deal.setAttribute('actual-url', imagesArray[selectedImage].actualurl);
-		deal.setAttribute('disclaimer', 'This image used without explicit permission.');
-		var title = document.createElement('span');
-		var titleContent = document.createElement('span');
-		var titleAdjectives = ['hipster', 'hand made', 'unforgettable', 'extravagant', 'unique', 'personalized', 'custom', 'unrivaled', 'fresh', 'stylish', 'premium', 'upgraded', 'exclusive', 'modern'];
-		var titleNouns = ['fried chicken', 'retreat', 'service', 'opportunity', 'luxury', 'experience', 'style', 'relaxation', 'package'];
+		var dealHeader = document.createElement('div');
+		dealHeader.className = 'deal-panel-header';
+		// dealHeader.style.backgroundImage = 'url(./assets/images/' + imagesArray[selectedImage].localurl + ')';
+		dealHeader.style.backgroundImage = 'url(/assets/images/' + imagesArray[selectedImage].localurl + ')';
+		dealHeader.setAttribute('actual-url', imagesArray[selectedImage].actualurl);
+		dealHeader.setAttribute('disclaimer', 'This image used without explicit permission.');
+		if(Math.random()<0.5){
+			var daysLeft = Math.floor(Math.random()*8)+2;
+			var limitedTime = document.createElement('div');
+			limitedTime.className = 'limited-time-container';
+			var ltText = document.createElement('div');
+			ltText.className = 'cornered-text-days cornered-text';
+			ltText.innerText = daysLeft;
+			var ltWords = document.createElement('div');
+			ltWords.className = 'cornered-text-words cornered-text';
+			ltWords.innerHTML = 'days left<br>to purchase';
+			var cornerOne = document.createElement('div');
+			cornerOne.className = 'cornered corner';
+			var cornerTwo = document.createElement('div');
+			cornerTwo.className = 'cornered2 corner';
+			limitedTime.appendChild(ltText);
+			limitedTime.appendChild(ltWords);
+			limitedTime.appendChild(cornerOne);
+			limitedTime.appendChild(cornerTwo);
+			dealHeader.appendChild(limitedTime);
+		}
+		deal.appendChild(dealHeader);
+		var dealContent = document.createElement('div');
+		dealContent.className = 'deal-panel-content';
+		//create title span
+		var dTitle = document.createElement('span');
+		dTitle.className = 'deal-title-content';
+		dTitle.innerText = titleAdj[Math.floor(Math.random()*titleAdj.length)] + ' ' + titleNoun[Math.floor(Math.random()*titleNoun.length)];
+		dealContent.appendChild(dTitle);
+		//create regular price div
+		var firstPrice = Math.floor(Math.random()*200) + 150;
+		var newPrice = Math.floor(((Math.random()*0.4)+0.5)*firstPrice);
+		var regPriceDiv = document.createElement('div');
+		regPriceDiv.className = 'deal-desc-div reg-price';
 		var origPrice = document.createElement('span');
 		var origPriceContent = document.createElement('span');
-		origPriceContent.style.textDecoration = 'line-through';
-		var price = document.createElement('span');
-		var priceContent = document.createElement('span');
-		//days left to purchase:
-		var daysLeft = document.createElement('span');
-		var daysLeftContent = document.createElement('span');
-
-//special "cut corners" effect. make a div hold the two cornered elements.
-//then have text in the parent div
-// 		<div class="cornered"></div>
-// <div class="main">Hello</div>
-// <div class="cornered2"></div>
-// 		.cornered {
-//     width: 160px;
-//     height: 0px;
-//     border-bottom: 40px solid green;
-//     border-right: 40px solid white;
-// }
-// .main {
-//     width: 200px;
-//     height: 20px;
-//     background-color: red;
-// }
-// .cornered2 {
-//     width: 160px;
-//     height: 0px;
-//     border-top: 40px solid green;
-//     border-right: 40px solid white;
-// }
-		//savings will be (originalPrice - price) / originalprice
-		var savings = document.createElement('span');
-		var savingsContent = document.createElement('span');
+		origPrice.className = 'deal-label';
+		origPrice.innerText = 'regular price: ';
+		origPriceContent.className = 'deal-regprice-content';
+		origPriceContent.innerText = '$' + firstPrice + '.00';
+		regPriceDiv.appendChild(origPrice);
+		regPriceDiv.appendChild(origPriceContent);
+		dealContent.appendChild(regPriceDiv);
+		//create now price div
+		var nowPriceDiv = document.createElement('div');
+		nowPriceDiv.className = 'deal-desc-div';
+		var nowPrice = document.createElement('span');
+		var nowPriceContent = document.createElement('span');
+		nowPrice.className = 'deal-label dl-dealprice';
+		nowPrice.innerText = 'NOW: ';
+		nowPriceContent.className = 'deal-dealprice-content';
+		nowPriceContent.innerText = '$' + newPrice + '.00';
+		nowPriceDiv.appendChild(nowPrice);
+		nowPriceDiv.appendChild(nowPriceContent);
+		// nowPriceDiv.innerHTML = nowPrice + nowPriceContent;
+		dealContent.appendChild(nowPriceDiv);
+		//create created at div
+		var createdAtDiv = document.createElement('div');
+		createdAtDiv.className = 'deal-desc-div ddd-cu';
 		var created = document.createElement('span');
 		var createdContent = document.createElement('span');
+		created.className = 'deal-label dl-cu';
+		created.innerText = 'deal created at: ';
+		createdContent.className = 'deal-created-content dl-cu';
+		createdContent.innerText = 'date created unknown';
+		createdAtDiv.appendChild(created);
+		createdAtDiv.appendChild(createdContent);
+		dealContent.appendChild(createdAtDiv);
+		//create updated at div
+		var updatedAtDiv = document.createElement('div');
+		updatedAtDiv.className = 'deal-desc-div ddd-cu';
 		var updated = document.createElement('span');
 		var updatedContent = document.createElement('span');
-
-		//div.innerText = arr[i].number;
-		//deals.innerHTML += dealPanel; appendChild(div)
+		updated.className = 'deal-label dl-cu';
+		updated.innerText = 'deal updated at: ';
+		updatedContent.className = 'deal-updated-content dl-cu';
+		updatedContent.innerText = 'date updated unknown';
+		updatedAtDiv.appendChild(updated);
+		updatedAtDiv.appendChild(updatedContent);
+		dealContent.appendChild(updatedAtDiv);
+		deal.appendChild(dealContent);
+		//savings will be (originalPrice - price) / originalprice
+		//var savings = document.createElement('span');
+		//var savingsContent = document.createElement('span');
 		document.getElementById('deal-container-ind').appendChild(deal);
 		imagesArray.splice(selectedImage, 1);
 	}
@@ -78,31 +123,31 @@ function populateIndBusDeals(howMany){
 
 var dealImages = [
 	{
-		localurl: '../image/deal_image_01.jpg',
+		localurl: 'deal_image_01.jpg',
 		actualurl:'http://www.hsalam-bamako.com/wp-content/uploads/2016/12/o-HAPPY-PEOPLE-facebook.jpg'
 	},
 	{
-		localurl: '../image/deal_image_02.jpg',
+		localurl: 'deal_image_02.jpg',
 		actualurl: 'http://youhaveacalling.com/wp-content/uploads/2012/05/Fun-Group-of-Young-People-Jump-19461599.jpg'
 	},
 	{
-		localurl: '../image/deal_image_03.jpg',
+		localurl: 'deal_image_03.jpg',
 		actualurl: 'http://www.siwallpaperhd.com/wp-content/uploads/2015/05/happy_people_colorful_wallpaper_hd_5.jpg'
 	},
 	{
-		localurl: '../image/deal_image_04.jpg',
+		localurl: 'deal_image_04.jpg',
 		actualurl: 'http://ultimate-luxury-community.com/wp-content/uploads/2011/10/MiSST-frontpage-red-girl-shopping-smaller-the-same.jpg'
 	},
 	{
-		localurl: '../image/deal_image_05.jpg',
+		localurl: 'deal_image_05.jpg',
 		actualurl: 'http://aplustexashomebuilders.com/wp-content/uploads/2015/09/luxury-homes-builders.jpg'
 	},
 	{
-		localurl: '../image/deal_image_06.jpg',
+		localurl: 'deal_image_06.jpg',
 		actualurl: 'http://www.eliteluxuryhomes.com/swserve/custom_themes/bootstrapeliteluxury/wctheme/images/home/location-beverly-hills.jpg'
 	},
 	{
-		localurl: '../image/deal_image_07.jpg',
+		localurl: 'deal_image_07.jpg',
 		actualurl: 'http://skylinegp.com/wp-content/uploads/2015/06/Chicago-Skyline.jpg'
 	}
 ];
